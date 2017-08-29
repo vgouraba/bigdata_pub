3da3e07b0c8dc60e9837f51838e5e78c62285a4c
Vasu Gourabathina <vgouraba@users.noreply.github.com>
2017-08-29 15:01:29 -0700

    Create wc2.py

---
+++ wc2.py
@@ -0,0 +1,30 @@
+# cluster-spark-wordcount.py
+from pyspark import SparkConf
+from pyspark import SparkContext
+from pyspark import SQLContext
+from pyspark.sql import Row
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
+HDFS_MASTER = 'quickstart.cloudera'
+
+conf = SparkConf()
+conf.setMaster('yarn-client')
+conf.setAppName('spark-wordcount')
+conf.set('spark.executor.instances', 10)
+sc = SparkContext(conf=conf)
+#sc2 = new SQLContext(sc);
+
+schema = StructType([ \
+           StructField("emp_no", IntegerType(), True), \
+           StructField("bdate", StringType(), True), \
+           StructField("fname", StringType(), True), \
+           StructField("lname", StringType(), True), \
+           StructField("gender", StringType(), True), \
+           StructField("hiredate", StringType(), True) ])
+
+sqlContext = SQLContext(sc)
+df = sqlContext.read.format("com.databricks.spark.csv").option("delimeter",",").option("header","false").option("inferSchema","true").load("hdfs://quickstart.cloudera:8020/home/cloudera/sqoop-import/employees/part-m-*", schema=schema)
+df.show(5)
+
+print "Done"
+df.select("fname").show(5)
