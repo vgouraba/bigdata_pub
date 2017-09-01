8e22a45a063be078bda4902a3ea58bf4a3007a96
Vasu Gourabathina <vgouraba@users.noreply.github.com>
2017-09-01 13:17:24 -0700

    Create sql_send.py

---
+++ sqs/sql_send.py
@@ -0,0 +1,23 @@
+import boto3
+
+def listQueues(sqs):
+  "Function to list all queueus"
+  response = sqs.list_queues()
+  return response
+
+def putMessage(sqs, queue_url):
+  response = sqs.send_message(
+    QueueUrl=queue_url,
+    #MessageGroupId='grp_1',
+    MessageBody=(
+        'Start the Process.'
+    )
+  )
+  print(response['MessageId'])
+
+sqs = boto3.client('sqs')
+
+queue = listQueues(sqs)
+print(queue['QueueUrls'])
+
+putMessage(sqs, 'https://us-west-2.queue.amazonaws.com/097884913175/bdt_daily')
